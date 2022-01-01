8472f02a9f8f49ae07025f010ca2aa22e614892c
Ammar <ammar.h.d@hotmail.com>
2022-01-01 22:14:52 +0100

    footer section

---
+++ components/footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full  mx-auto mt-10 md:mt-20   max-w-screen-md">
+      <p className="py-8 text-sm font-light text-center dark:text-white-700 text-black-700">
+        <a
+          href="https://github.com/ammarhd"
+          target="_blank"
+          className="hover:opacity-80 transition-opacity"
+          rel="noreferrer"
+        >
+          Designed & Crafted by Ammar © 2021
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
