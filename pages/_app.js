6e251f3b26a323cabbc714d3ad24663f6c1209be
Ammar <ammar.h.d@hotmail.com>
2022-01-02 11:14:54 +0100

    google amalytics

--- pages/_app.js
+++ pages/_app.js
@@ -9,11 +9,12 @@ function Website({ Component, pageProps, router }) {
     <>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
+        id="script1"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script id="script2" strategy="lazyOnload">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
